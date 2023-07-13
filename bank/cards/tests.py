b786d369842bd468c1524b1900ddb3cc8f94cffa
Sasha Niiakyi <etrigan070@gmail.com>
2023-07-13 22:11:07 +0300

    Added a test for post request
    Checked to create card to db

--- bank/cards/tests.py
+++ bank/cards/tests.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from cards.models import Card
 from django.urls import reverse
 import json
+import uuid
+from datetime import datetime
 
 
 class CardsTest(TestCase):
@@ -28,28 +30,41 @@ class CardsTest(TestCase):
 		self.assertEquals(expected_result, response.content.decode('utf-8'))
 
 
-	# def test_post_card(self):
-	# 	# given
-	# 	data = json.dumps({
-	# 						"number": 1111222233334455,
-	# 						"expir_date": "2025-11-01",
-	# 						"cvv": 123
-	# 					})
+	def test_post_card(self):
+		# given
+		data = {"number": 1111222233334455,
+				"expir_date": "2025-11-01",
+				"cvv": 123,}
 
-	# 	url = reverse('card')
+		url = reverse('create_card')
 
-	# 	# when
-	# 	response = self.client.post(url, data=data, content_type='application/json')
+		# when
+		response = self.client.post(url, data=data)
 
-	# 	card = Card.objects.filter(number=1111222233334455)[0]
+		# template
+		actual_template = 'cards/create.html'
+		context = {'title': 'Create card', 'check': False}
 
-	# 	# then
-	# 	self.assertEquals(response.json(), {'card': {'number': card.number,
-	# 	                                      'expir_date': str(card.expir_date),
-	# 	                                      'cvv': card.cvv,
-	# 	                                      'date_of_issue': str(card.date_of_issue), 
-	# 	                                      'user_id': str(card.user_id),
-	# 	                                      'status': card.status}})
+		expected_result = render_to_string(actual_template, context)
+
+		# db
+		card = Card.objects.filter(number=1111222233334455).first()
+
+		expect_data = {"number": 1111222233334455,
+						"expir_date": "2025-11-01",
+						"cvv": 123,
+						'date_of_issue': str(datetime.now().date()), 
+						'user_id': True,
+						'status': 'new'}
+
+		# then
+		self.assertEquals(expected_result, response.content.decode('utf-8'))
+		self.assertEquals(expect_data, {'number': card.number,
+										'expir_date': str(card.expir_date),
+										'cvv': card.cvv,
+										'date_of_issue': str(card.date_of_issue), 
+										'user_id': self.is_valid_uuid(str(card.user_id)),
+										'status': card.status})
 
 
 	def test_is_valid_false(self):
@@ -64,4 +79,14 @@ class CardsTest(TestCase):
 
 		result = card.is_valid()
 		self.assertEquals(result, True)
+
+
+	def is_valid_uuid(self, check_uuid: str):
+		try:
+			uuid_obj = uuid.UUID(check_uuid)
+			is_valid_uuid = True
+		except ValueError:
+			is_valid_uuid = False
+
+		return is_valid_uuid
 
